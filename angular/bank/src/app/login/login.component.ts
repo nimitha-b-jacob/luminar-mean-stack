99a192e52f58911f771e43f77c836fdce67fe399
nimitha <nimithaabjacob@gmail.com>
2021-05-18 22:15:43 +0530

    1 way binding

---
+++ angular/bank/src/app/login/login.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  ab="welcome"
+  accno="account no please"
+  pwd=""
+  accountdetails:any = {
+    1000: {
+      acno: 1000,
+      username: "userone",
+      password: "userone",
+      balance: 50000,
+    },
+    1001: {
+      acno: 1001,
+      username: "usertwo",
+      password: "usertwo",
+      balance: 5000,
+    },
+    1002: {
+      acno: 1002,
+      username: "userthree",
+      password: "userthree",
+      balance: 10000,
+    },
+    1003: {
+      acno: 1003,
+      username: "userfour",
+      password: "userfour",
+      balance: 6000,
+    },
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  /*accnoChange(event:any)
+  {
+    this.accno=event.target.value;
+    console.log(this.accno);
+    
+  }
+  pswChange(event:any)
+  {
+    this.pwd=event.target.value;
+    console.log(this.pwd);
+    
+  }*/
+  login(a:any,p:any)
+  {
+    var acno=a.value
+    var pwd=p.value
+    let users = this.accountdetails
+  
+    if (acno in users) 
+    {
+      if ((pwd == users[acno]["password"])) 
+      {
+        
+        alert("success")
+      }
+      else
+      {
+      alert("incorrect pswd")
+        }
+    }  
+     else 
+     {
+       
+      alert("no account")
+    }
+  }
+}
